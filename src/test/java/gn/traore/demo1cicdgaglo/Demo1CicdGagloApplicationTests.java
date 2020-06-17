811d401dd65eb0fae70fe5f0c581dc087a463f02
Ibrahima <ibrahima982011@koonda.fr>
2020-06-17 22:17:37 +0000

    Ecriture des tests unitaires pour la demo.

--- src/test/java/gn/traore/demo1cicdgaglo/Demo1CicdGagloApplicationTests.java
+++ src/test/java/gn/traore/demo1cicdgaglo/Demo1CicdGagloApplicationTests.java
@@ -3,11 +3,27 @@ package gn.traore.demo1cicdgaglo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class Demo1CicdGagloApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void assertThatOnePlusOneIsTwo() {
+		int somme = 1 + 1;
+		assertThat(somme).isNotNull();
+		assertThat(somme).isEqualTo(2);
+	}
+
+	@Test
+	void assertThatOnePlusTwoIsThree() {
+		int somme = 1 + 2;
+		assertThat(somme).isNotNull();
+		assertThat(somme).isEqualTo(2);
 	}
 
 }
