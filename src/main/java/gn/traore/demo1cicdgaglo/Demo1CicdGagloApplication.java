4f4e8a3a63ca81e22acfd4cc02fbcd848cd389c6
Ibrahima <ibrahima982011@koonda.fr>
2020-06-17 02:03:59 +0000

    Initialisation du projet

---
+++ src/main/java/gn/traore/demo1cicdgaglo/Demo1CicdGagloApplication.java
@@ -0,0 +1,13 @@
+package gn.traore.demo1cicdgaglo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1CicdGagloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1CicdGagloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gn/traore/demo1cicdgaglo/Demo1CicdGagloApplicationTests.java
@@ -0,0 +1,13 @@
+package gn.traore.demo1cicdgaglo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1CicdGagloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
