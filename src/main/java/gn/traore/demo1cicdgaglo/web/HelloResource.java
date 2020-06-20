450b3262e2273f3c7c3e57c30f225e1f77c62433
Ibrahima <ibrahima982011@koonda.fr>
2020-06-20 05:03:28 +0000

    Ajout d'un endpoint dans le RestController pour tester l'APP dans le container.

--- src/main/java/gn/traore/demo1cicdgaglo/web/HelloResource.java
+++ src/main/java/gn/traore/demo1cicdgaglo/web/HelloResource.java
@@ -1,13 +1,19 @@
 package gn.traore.demo1cicdgaglo.web;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HelloResource {
 
     @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello my DEAR TRAORE";
+    public ResponseEntity sayHello() {
+        Map<String, String> data = new HashMap<>();
+        data.put("Hello", "Hello my dear TRAORE");
+        return ResponseEntity.ok(data);
     }
 }
